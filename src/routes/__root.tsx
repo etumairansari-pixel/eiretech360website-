b193bc2e4af916d3f655a395014733dd08cab8d3
Eire Tech <eiretech360@gmail.com>
2026-07-14 14:52:44 -0700

    Remove PWA manifest links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -144,7 +144,6 @@ export const Route = createRootRouteWithContext<Record<string, never>>()({
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
       { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-      { rel: "manifest", href: "/site.webmanifest" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "preload", as: "style", href: FONTS_HREF },
